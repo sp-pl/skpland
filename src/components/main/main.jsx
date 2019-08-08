a5a9abceb9f3540f6d34fe117ae08df61721d658
artur_arczi <artjuzwik93@gmail.com>
2019-08-08 11:59:31 +0200

    cd

---
+++ src/components/main/main.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Main extends React.Component{
+	constructor(){
+		super()
+	};
+
+	render(){
+		return(
+			<div>
+				
+
+
+				duasjdsalfnfa
+			</div>
+		);
+	}
+}
+
+export default Main;
